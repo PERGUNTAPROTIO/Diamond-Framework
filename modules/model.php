3526dc0653305495d27bc7edcdf3d6daf7f59ae9
PERGUNTAPROTIO <canaldoperguntaprotio@gmail.com>
2016-01-21 15:37:42 -0200

    Mais Reajustes
    Um pouco mais de reajustes nas pastas do projeto

--- modules/model.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	class Model {
-		public function getConectionDataBase($host, $dbname, $user, $password) {
-			$get = new PDO("mysql:host={$host};dbname={$dbname}", $user, $password);
-		}
-	} 
